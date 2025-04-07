560a852473c7f318cac800b77466c757f024af35
Ashish Patel <apatel2@ualberta.ca>
2025-04-06 23:56:48 -0600

    feat: update dashboard layout with cloud decorations

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,15 +10,21 @@ export default function DashboardLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className="min-h-screen">
-        <div className="flex">
+      <div className="relative min-h-screen overflow-hidden">
+        {/* Decorative clouds */}
+        <div className="ghibli-cloud ghibli-cloud-1"></div>
+        <div className="ghibli-cloud ghibli-cloud-2"></div>
+        <div className="ghibli-cloud ghibli-cloud-3"></div>
+
+        <div className="flex min-h-screen">
           <AppSidebar />
           <div className="flex flex-col flex-1">
             <TopNavigation />
-            <main className="flex-1 p-4">{children}</main>
+            <main className="flex-1 p-4 md:p-6">{children}</main>
           </div>
         </div>
       </div>
     </SidebarProvider>
   )
 }
+
