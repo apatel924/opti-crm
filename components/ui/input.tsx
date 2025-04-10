14dcfcd1dd78cc605972314d1cc5661e4593328f
Ashish Patel <apatel2@ualberta.ca>
2025-04-09 23:55:56 -0600

    Update input component styling

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,16 +1,23 @@
 import * as React from "react"
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
+import { cn } from "@/lib/utils"
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        "flex h-10 w-full rounded-xl border-2 border-ghibli-blue-light bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ghibli-blue focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-300 hover:border-ghibli-blue",
+        className,
+      )}
+      ref={ref}
+      {...props}
+    />
+  )
+})
 Input.displayName = "Input"
 
 export { Input }
+
