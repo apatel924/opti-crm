eeebef228135f4e3c1b470082c4f4533c8e28dc2
Ashish Patel <apatel2@ualberta.ca>
2025-04-24 01:22:11 -0600

    initial

---
+++ components/patients/patient-profile-page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useState } from "react"
+import Link from "next/link"
+
+interface PatientProfilePageProps {
+  patientId: string
+}
+
+export function PatientProfilePage({ patientId }: PatientProfilePageProps) {
+  return <div>Patient Profile Page</div>
+}
