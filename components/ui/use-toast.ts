fc1ab533206847b1089118b722170cdd42205195
Ashish Patel <apatel2@ualberta.ca>
2025-05-05 23:56:20 -0600

    1

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -8,6 +8,22 @@ import type {
   ToastProps,
 } from "@/components/ui/toast"
 
+const TOAST_LIMIT = 1
+const TOAST_REMOVE_DELAY = 1000000
+
+const actionTypes = {
+  ADD_TOAST: "ADD_TOAST",
+  UPDATE_TOAST: "UPDATE_TOAST",
+  DISMISS_TOAST: "DISMISS_TOAST",
+  REMOVE_TOAST: "REMOVE_TOAST",
+} as const
+
+let count = 0
+function genId() {
+  count = (count + 1) % Number.MAX_SAFE_INTEGER
+  return count.toString()
+}
+
 type ToasterToast = ToastProps & {
   id: string
   title?: React.ReactNode
