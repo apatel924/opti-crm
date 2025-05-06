008936b8d1179ac1513fcfc643382ef9f2d73945
Ashish Patel <apatel2@ualberta.ca>
2025-05-05 23:58:38 -0600

    toast api

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -108,3 +108,31 @@ function dispatch(action: Action) {
   memoryState = reducer(memoryState, action)
   listeners.forEach((listener) => listener(memoryState))
 }
+
+type Toast = Omit<ToasterToast, "id">
+
+export function toast({ ...props }: Toast) {
+  const id = genId()
+
+  const update = (props: ToasterToast) =>
+    dispatch({ type: "UPDATE_TOAST", toast: { ...props, id } })
+  const dismiss = () => dispatch({ type: "DISMISS_TOAST", toastId: id })
+
+  dispatch({
+    type: "ADD_TOAST",
+    toast: {
+      ...props,
+      id,
+      open: true,
+      onOpenChange: (open) => {
+        if (!open) dismiss()
+      },
+    },
+  })
+
+  return {
+    id,
+    dismiss,
+    update,
+  }
+}
