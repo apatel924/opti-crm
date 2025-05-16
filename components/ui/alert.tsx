cbd1573dcf90fae20f14a4edbc838892a5b5393e
Ashish Patel <apatel2@ualberta.ca>
2025-05-15 20:39:26 -0600

    1

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -32,5 +32,28 @@ const Alert = React.forwardRef<
 ))
 Alert.displayName = "Alert"
 
+const AlertTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h5
+    ref={ref}
+    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+    {...props}
+  />
+))
+AlertTitle.displayName = "AlertTitle"
 
-export { Alert}
+const AlertDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    {...props}
+  />
+))
+AlertDescription.displayName = "AlertDescription"
+
+export { Alert, AlertTitle, AlertDescription }
